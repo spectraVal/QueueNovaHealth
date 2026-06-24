2d158aa8ad7d1060303ba786db1c8ded30672e2e
Hanchan <handikacp155@gmail.com>
2026-06-24 15:35:02 +0700

    fix docker file for backend, change fe allowed port from 5173 to 3000

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -21,7 +21,8 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
+//    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
 
     'allowed_origins_patterns' => [],
 
